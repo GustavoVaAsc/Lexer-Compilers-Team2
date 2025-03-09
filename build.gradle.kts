78b6c388e7f83932f508462a280c0f10eca486e6
GustavoVaAsc <quickgohan98@gmail.com>
2025-03-09 15:32:13 -0600

    I add the Android Project structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,42 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+android {
+    compileSdk = 34
+    namespace = "unam.fi.compilers.lexer"
+    defaultConfig {
+        applicationId = "unam.fi.compilers.lexer"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    sourceSets {
+        getByName("main").manifest.srcFile("app/src/main/AndroidManifest.xml")
+    }
+
 }
+
+plugins {
+    alias(libs.plugins.android.application) apply true
+    alias(libs.plugins.kotlin.android) apply true
+    alias(libs.plugins.kotlin.compose) apply true
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)  // Correct key from TOML for AppCompat
+    implementation(libs.material)           // Correct key from TOML for Material Design
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.gradle)  // Make sure this matches your AGP version
+    }
+}
+
+
